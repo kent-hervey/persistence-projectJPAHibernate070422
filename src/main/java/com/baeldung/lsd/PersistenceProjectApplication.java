3511524aebf924081add98d13fe40cd39f313044
Kent Hervey <kent.hervey@gmail.com>
2022-07-06 17:45:09 -0500

    autowired projectRepository

--- src/main/java/com/baeldung/lsd/PersistenceProjectApplication.java
+++ src/main/java/com/baeldung/lsd/PersistenceProjectApplication.java
@@ -2,25 +2,29 @@ package com.baeldung.lsd;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class PersistenceProjectApplication implements ApplicationRunner {
 
+	@Autowired
+	private ProjectRepository projectRepository;
+
 	private static final Logger LOG = LoggerFactory.getLogger(PersistenceProjectApplication.class);
 
 	public static void main(String[] args) {
-		final ConfigurableApplicationContext run = SpringApplication.run(PersistenceProjectApplication.class, args);
+		SpringApplication.run(PersistenceProjectApplication.class, args);
 	}
 
-		@Override
-		public void run(ApplicationArguments args) throws Exception {
-			LOG.info("Starting Spring Boot application...");
-		}
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("Missouri");
+		Iterable<Project> allProjects = projectRepository.findAll();
+		LOG.info("All Projects:\n{}", allProjects);
+		System.out.println("Texas");
 	}
-
-
+}
