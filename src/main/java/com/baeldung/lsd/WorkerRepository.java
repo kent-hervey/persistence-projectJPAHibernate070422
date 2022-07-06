e0bb073001f865f749f01b27a91120b61cfade2f
Kent Hervey <kent.hervey@gmail.com>
2022-07-06 17:49:51 -0500

    changed Project to worker as type argument

--- src/main/java/com/baeldung/lsd/WorkerRepository.java
+++ src/main/java/com/baeldung/lsd/WorkerRepository.java
@@ -2,5 +2,5 @@ package com.baeldung.lsd;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface WorkerRepository extends CrudRepository< Project, Long> {
+public interface WorkerRepository extends CrudRepository< Worker, Long> {
 }
